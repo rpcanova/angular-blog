ada69df0837bd842a227460da29b87944d1c4ff6
unknown <rpcanova@hotmail.com>
2024-11-19 20:02:51 -0300

    dynamic components properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+    @Input() pictureCover: string = ""
+    @Input() cardTitle: string = ""
+    @Input() cardDescription: string = ""
 
+    constructor() { }
 }
